a28f3a8fca8e52478c3800bd1117b5392f4e2a70
Tie-dan <415045745@qq.com>
2020-02-12 18:17:16 +0800

    change

--- vue项目/vue-topline/笔记/02day/vue-pro/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  lintOnSave: true, // 文件保存时就做eslint规范检测
-  devServer: {// 实时保存、编译的配置段
-    open: true, // 自动开启浏览器
-    port: 12306, // 服务运行端口
-    host: '127.0.0.1' // 运行服务的主机名
-  }
-}
